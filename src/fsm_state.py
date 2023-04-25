4291941d7c0d2346e45cfa5b6e9b5832efaa2179
Jarumi Hooi <jarumihooi@gmail.com>
2023-04-25 22:17:56 +0000

    updates halfway

--- src/fsm_state.py
+++ src/fsm_state.py
@@ -16,59 +16,71 @@
     t.stuck() # etc
     t.get_graph().draw('fsm_example.png', prog='dot')'''
 
+import rospy
 from transitions import Machine, State
 import random
 from transitions.extensions import GraphMachine
+from std_msgs.msg import Bool, String
 
 class FSM(object):
     def __init__(self):
-        rospy.init_node('fsm_state')
+        rospy.init_node('fsm')
+        self.rate = rospy.Rate(30)
         self.objects_left = 1
 
         # pubs
         self.state_sub = rospy.Publisher("/state", String, queue_size=1)
+
+        # init the fsm 
+        states = [
+            State(name='Goto_loading', on_enter="chg_state"),
+            'Cv_items', 
+            'Pickup',
+            'Goto_drop', 
+            State(name='Drop', on_exit="dropped_off_cb"),
+            'Shutdown'
+        ]
+        transitions = [
+            {"trigger":"at_loading", 'source':'Goto_loading', 'dest':'Cv_items', 'before': 'remaining_cb'},
+            { 'trigger': 'inpos_pickup', 'source': 'Cv_items', 'dest': 'Pickup', 'after': 'pickedup' },
+            { 'trigger': 'deliver_goto', 'source': 'Pickup', 'dest': 'Goto_drop' },
+            { 'trigger': 'inpos_drop', 'source': 'Goto_drop', 'dest': 'Drop' },
+            { 'trigger': 'reload', 'source': 'Drop', 'dest': 'Goto_loading' },
+            { 'trigger': 'end', 'source': 'Drop', 'dest': 'Shutdown' }
+        ]
+        machine = GraphMachine(model=self, states=states, 
+            transitions=transitions, initial="Goto_loading")
+        # end init the fsm ====
    
+    # end init()
 
     # these are the callbacks for when a state changes. they must be applied to the states directly. 
     def remaining_cb(self): print("objects_left", self.objects_left)
     def pickedup(self):print("after: we picked up a can")
     def dropped_off_cb(self): self.objects_left -= 1; print("objects_left", self.objects_left)
 
-# =====
-def main():
-    fsm = FSM()
-    states = [
-        State(name='Goto_loading', on_enter="chg_state"),
-        'Cv_items', 
-        'Pickup',
-        'Goto_drop', 
-        State(name='Drop', on_exit="dropped_off_cb"),
-        'Shutdown'
-    ]
-    transitions = [
-        {"trigger":"at_loading", 'source':'Goto_loading', 'dest':'Cv_items', 'before': 'remaining_cb'},
-        { 'trigger': 'inpos_pickup', 'source': 'Cv_items', 'dest': 'Pickup', 'after': 'pickedup' },
-        { 'trigger': 'deliver_goto', 'source': 'Pickup', 'dest': 'Goto_drop' },
-        { 'trigger': 'inpos_drop', 'source': 'Goto_drop', 'dest': 'Drop' },
-        { 'trigger': 'reload', 'source': 'Drop', 'dest': 'Goto_loading' },
-        { 'trigger': 'end', 'source': 'Drop', 'dest': 'Shutdown' }
-    ]
+    def run(self):
+        print("starting fsm")
+        print("fsm thinks state is", self.state)
+        while not rospy.is_shutdown():
+            self.rate.sleep()
 
-    machine = GraphMachine(model=fsm, states=states, 
-        transitions=transitions, initial="Goto_loading")
+# =====
+# if __name__ == '__main__':
+#     fsm = FSM()
+   
     # bf: was missing transitions=transitions, this creates the methods
     
 
 
     # do section ==============
-    fsm.get_graph().draw('fsm_state_diag.png', prog='dot')
-    print(fsm.state)
-    fsm.at_loading()
-    print(fsm.state)
+    # fsm.get_graph().draw('fsm_state_diag.png', prog='dot')
+    # print(fsm.state)
+    # fsm.at_loading()
+    # print(fsm.state)
     # fsm.evaporate()
     # print(fsm.state)
     # fsm.deposit()
     # print(fsm.state)
     
 
-main()
