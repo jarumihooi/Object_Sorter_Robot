8ead6d2f2087c806569e515ab9394cf3f6f3eb36
Jarumi Hooi <jarumihooi@gmail.com>
2023-04-25 03:38:25 +0000

    finished fsm_demo2.py

--- src/fsm_demo2.py
+++ src/fsm_demo2.py
@@ -1,30 +1,44 @@
-# jeremy attempt at simple fsm
-# Make sure the package is installed: pip install transitions; pip install transitions[diagrams]  
-#
-# In ipython:
-#     from fsm_example import MoveBaseRecovery
-#     t = MoveBaseRecovery("base")
-#     t.state
-#     t.stuck() # etc
-#     t.get_graph().draw('fsm_example.png', prog='dot')
+#!/usr/local/bin/python
+#!/usr/bin/python
 
-from transitions import Machine
+'''Info: jeremy attempt at simple fsm
+    
+    Make sure the package is installed: pip install transitions; pip install transitions[diagrams]  
+    info list: https://github.com/pytransitions/transitions
+
+    In ipython:
+    from fsm_example import MoveBaseRecovery
+    t = MoveBaseRecovery("base")
+    t.state
+    t.stuck() # etc
+    t.get_graph().draw('fsm_example.png', prog='dot')'''
+
+from transitions import Machine, State
 import random
 
 class Matter(object):
     # i think this creates a super class
-    def chg_state(): print("changing state")
+
+    # these are the callbacks for when a state changes. they must be applied to the states directly. 
+    def chg_state(self): print("changing state")
+    def on_enter_gas(self):print("callback for entering gas")
 
 # =====
 def main():
     lump = Matter()
+    states = [State(name='solid', on_enter="chg_state"), 'liquid', 'gas', 'plasma']
     transitions = [
         {"trigger":"melt", 'source':'solid', 'dest':'liquid'},
         { 'trigger': 'evaporate', 'source': 'liquid', 'dest': 'gas' },
         { 'trigger': 'sublimate', 'source': 'solid', 'dest': 'gas' },
-        { 'trigger': 'ionize', 'source': 'gas', 'dest': 'plasma' }
+        { 'trigger': 'ionize', 'source': 'gas', 'dest': 'plasma' },
+
+        { 'trigger': 'de-ionize', 'source': 'plasma', 'dest': 'gas' },
+        { 'trigger': 'condensate', 'source': 'gas', 'dest': 'liquid' },
+        { 'trigger': 'freeze', 'source': 'liquid', 'dest': 'solid' },
+        { 'trigger': 'deposit', 'source': 'gas', 'dest': 'solid' }
     ]
-    machine = Machine(model=lump, states=['solid', 'liquid', 'gas', 'plasma'], 
+    machine = Machine(model=lump, states=states, 
         transitions=transitions, initial="solid")
     # bf: was missing transitions=transitions, this creates the methods
     
@@ -34,5 +48,10 @@ def main():
     print(lump.state)
     lump.melt()
     print(lump.state)
+    lump.evaporate()
+    print(lump.state)
+    lump.deposit()
+    print(lump.state)
+
 
 main()
