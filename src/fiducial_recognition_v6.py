7b5b99ea7355094928a5dd9cd14ba2ad06d36e81
Jarumi Hooi <jarumihooi@gmail.com>
2023-05-02 20:39:50 +0000

    adding v6 to branch fsm

---
+++ src/fiducial_recognition_v6.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import TransformStamped, Twist
+from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
+from diagnostic_msgs.msg import DiagnosticArray
+import tf2_ros
+from math import pi, sqrt, atan2
+import traceback
+import math
+import time
+from std_msgs.msg import Bool, String
+
+def degrees(r):
+    return 180.0 * r / math.pi
+
+class Follow:
+    """
+    Constructor for our class
+    """
+    def __init__(self):
+       rospy.init_node('follow')
+
+       # Set up a transform listener so we can lookup transforms in the past
+       self.tfBuffer = tf2_ros.Buffer(rospy.Time(30))
+       self.lr = tf2_ros.TransformListener(self.tfBuffer)
+
+       # Setup a transform broadcaster so that we can publish transforms
+       # This allows to visualize the 3D position of the fiducial easily in rviz
+       self.br = tf2_ros.TransformBroadcaster()
+
+       # A publisher for robot motion commands 
+       self.fid_pub = rospy.Publisher("/fid_pub", TransformStamped, queue_size=1) #TODO list or tuple?
+
+       # Subscribe to incoming transforms
+       rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.newTf)
+
+
+
+    """
+    Called when a FiducialTransformArray is received
+    """
+
+    def newTf(self, msg):
+        imageTime = msg.header.stamp
+
+        print (imageTime, rospy.Time.now())
+        print ("*****")
+
+        finalTransforms = {}
+
+
+        # For every fiducial found by the dectector, publish a transform
+        for m in msg.transforms:
+            id = m.fiducial_id
+            trans = m.transform.translation
+            rot = m.transform.rotation
+            print ("Fid %d %lf %lf %lf %lf %lf %lf %lf\n" % \
+                                 (id, trans.x, trans.y, trans.z,
+                                  rot.x, rot.y, rot.z, rot.w))
+            t = TransformStamped()
+            t.child_frame_id = "fid%d" % id
+            t.header.frame_id = msg.header.frame_id
+            t.header.stamp = imageTime
+
+            t.transform.translation.x = trans.z #x to z
+            t.transform.translation.y = trans.x # y to x
+            t.transform.translation.z = trans.y # z to y
+
+            t.transform.rotation.x = rot.x
+            t.transform.rotation.y = rot.y
+            t.transform.rotation.z = rot.z
+            t.transform.rotation.w = rot.w
+            self.br.sendTransform(t)
+            print(t)
+            self.fid_pub.publish(t)
+
+
+
+
+            
+
+
+
+
+
+
+
+    """
+    Main loop
+    """
+    def run(self):
+        # setup for looping at 20hz
+        rate = rospy.Rate(20)
+
+
+        # While our node is running
+        while not rospy.is_shutdown():
+            # Calculate the error in the x and y directions
+
+
+
+
+            #publish result here
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    # Create an instance of our follow class
+    node = Follow()
+    # run it
+    node.run()
---
+++ src/image_recognition/06_contour_image_rec_v6.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+import rospy, cv2, cv_bridge
+from sensor_msgs.msg import CompressedImage, Image
+from geometry_msgs.msg import Twist
+import math
+import numpy as np
+from std_msgs.msg import Bool, String
+
+from image_tools import ImageTools
+
+
+def ang_vel_control(x):
+    return -1/(1+math.e**x) + 0.5
+
+
+
+class Follower:
+    def __init__(self):
+        self.bridge = cv_bridge.CvBridge()
+        #self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
+        self.image_sub = rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, self.image_callback, buff_size=2**24)
+        #self.image_sub = rospy.Subscriber('/raspicam_node/image', Image, self.image_callback)
+
+        self.centroid_pub = rospy.Publisher('centroid', CompressedImage, queue_size=1)
+        self.direction_pub = rospy.Publisher('/color_direction_twist', Twist, queue_size=1)
+
+        #TODO for testing only, remove after
+        self.servo_pub = rospy.Publisher('/servo', Bool, queue_size=1)#publishes commands to the claw
+
+        self.color_pub = rospy.Subscriber('/color', String, self.set_color_cb)
+        self.target_color = "green"
+
+
+        self.twist = Twist()
+        self.logcount = 0
+        self.lostcount = 0
+
+    def set_color_cb(self, msg):
+        self.target_color = msg.data
+        print(self.target_color)
+
+    def image_callback(self, msg):
+        # print('img_callback')
+
+        #to prevent errors in the print statement when there are no contours on screen
+        w=0
+
+        #self.servo_pub.publish(True)
+
+        converter = ImageTools()
+
+        imagemsg = converter.convert_to_ros_msg(msg)
+
+        # get image from camera
+        image = self.bridge.imgmsg_to_cv2(imagemsg)
+
+        # np_arr = np.fromstring(msg.data, np.uint8)
+        # image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        # filter out everything that's not yellow
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_yellow = np.array([ 40, 0, 0]) 
+        upper_yellow = np.array([ 120, 255, 255])
+
+        #ideal color is 0, 100, 95 #prevously 160-180
+        lower_red = np.array([170,80,80])
+        upper_red = np.array([190,255,255])
+
+            #50-90 ideal
+        lower_green = np.array([30,80,80])
+        upper_green = np.array([85,255,255])
+        
+        lower_target = lower_green
+        upper_target = upper_green
+
+
+        if self.target_color == "red":
+            lower_target = lower_red
+            upper_target = upper_red
+
+
+        # the mask
+        mask = cv2.inRange(hsv, lower_target, upper_target)
+        output = cv2.bitwise_and(image, image, mask=mask)
+
+        cv2.imshow("mask", mask)
+
+        cx = 0
+        cy = 0
+
+        ret,thresh = cv2.threshold(mask, 40, 255, 0)
+        if (int(cv2.__version__[0]) > 3):
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        else:
+            im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        if len(contours) != 0:
+            # draw in blue the contours that were founded
+            cv2.drawContours(output, contours, -1, 255, 3)
+
+            # find the biggest countour (c) by the area
+            c = max(contours, key = cv2.contourArea)
+            x,y,w,h = cv2.boundingRect(c)
+
+            cx = x + (w/2)
+            cy = y + (h/2)
+
+            # draw the biggest contour (c) in green
+            cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+
+        # show the images
+        cv2.circle(image, (int(cx), int(cy)), 8, (255,255,0), -1)
+
+        print("rect width:   " + str(w))
+
+        #following test**********************
+        h2, w2, d2 = image.shape
+        
+        #sets unused twist value to one to signal main file to close claw once close enough
+        #ideal value so far is 220
+        if(w > 200):
+            self.twist.linear.y = 1.0
+        else:
+            self.twist.linear.y = 0
+
+        
+        err = cx - w2/2
+       #self.twist.linear.x = 0.2
+        ang_vel = ang_vel_control(-float(err) / 100)
+
+        # print("ang_vel= "+str(ang_vel)) #I turned this off for launch testing too. 
+            
+        self.twist.angular.z = ang_vel
+        self.direction_pub.publish(self.twist) 
+        #*************************
+
+        cv2.imshow("Result", np.hstack([image, output]))
+        cv2.waitKey(3)
+
+
+print('running')
+rospy.init_node('follower')
+follower = Follower()
+rospy.spin()
